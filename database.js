2d45e102240900d789575b939af65964c3d343f9
bprcode <bprcode@gmail.com>
2023-08-21 11:19:43 -0700

    Order note list by creation time

--- database.js
+++ database.js
@@ -131,7 +131,8 @@ async function updateNote({ content, title, noteId, authorId }) {
 async function listNotes(author) {
   log('getting note list for ', blue, author)
   const result = await pool.query(
-    'SELECT note_id, title, summary FROM notes WHERE author_id = $1::text',
+    'SELECT note_id, title, summary FROM notes WHERE author_id = $1::text '
+    + 'ORDER BY created_at',
     [author]
   )
 
